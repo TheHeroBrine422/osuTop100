6315af9d3ac89d0a41504de324b432724ab02941
TheHeroBrine422 <caleb@calebgj.io>
2020-11-24 12:53:36 -0600

    starter code

---
+++ api/main.js
@@ -0,0 +1,43 @@
+// install: npm install express axios
+// options/info
+port = 3002
+osuid = "11361720"
+
+// require libraries
+const express = require('express')
+const axios = require('axios')
+
+// setup clients
+const app = express();
+
+app.get('/osuTop100/', (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+  output = "error: something went wrong. Check server log."
+
+  axios.get('https://osu.ppy.sh/users/'+osuid+'/scores/best?mode=mania&offset=0&limit=50')
+    .then(function (response) {
+      output = response.data
+
+      axios.get('https://osu.ppy.sh/users/'+osuid+'/scores/best?mode=mania&offset=50&limit=50')
+        .then(function (response) {
+          for (var i = 0; i < response.data.length; i++) {
+            output.push(response.data[i])
+          }
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+        .then(function () {
+          res.send(JSON.stringify(output))
+        })
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
+});
+
+app.listen(port, () => {
+  console.log('server started at localhost:'+port);
+});
